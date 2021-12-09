4f49acc56dafab32634f74b620108d656cae04e1
Ayselquliyeva <92804962+Ayselquliyeva@users.noreply.github.com>
2021-12-09 17:12:38 +0400

    Add files via upload

---
+++ 7/script.js
@@ -0,0 +1,11 @@
+function Gonder(say){
+    let ekran =document.getElementById("ekran");
+    ekran.value += say
+}
+ onload=function Goster(){
+    let show =document.getElementById("show").value;
+}
+show.innerHTML=eval(value)
+    
+    
+
